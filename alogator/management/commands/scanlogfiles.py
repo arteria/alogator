896db786f30a7642bc67306c44fc05f037be875e
Philippe O. Wagner <wagner@arteria.ch>
2013-11-07 10:28:58 +0100

    no message

--- alogator/management/commands/scanlogfiles.py
+++ alogator/management/commands/scanlogfiles.py
@@ -2,6 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 from alogator.logwatch import logWatcher
 
 class Command(BaseCommand):
+    args = ''
+    help = 'Scan logfiles for patterns.'
 
-    def handle(self):
+    def handle(self, *args, **options):
+        self.stdout.write('Scanning logfiles...')
         logWatcher()
+        self.stdout.write('Done. Goodbye.')
+            
+ 
+        
